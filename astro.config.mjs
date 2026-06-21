0f68906b060f303c9d90e68958508f785603e68a
Joseph Stinton <josephstinton@Josephs-MacBook-Pro-2.local>
2026-06-21 16:11:03 -0500

    Add custom domain: thisisfernandoduarte.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: process.env.SITE || 'https://jstinton.github.io',
-  base: process.env.BASE_PATH || '/fernando-duarte',
+  site: process.env.SITE || 'https://thisisfernandoduarte.com',
   integrations: [tailwind()],
   output: 'static',
   image: {
